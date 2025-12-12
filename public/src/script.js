d3ee02c0fde4e8c15c9c67ab35754f610d744c09
Satyaranjan7777 <satyaranjan123@gmail.com>
2025-12-12 22:19:29 +0530

    UI updated + bug fixes

---
+++ public/src/script.js
@@ -0,0 +1,53 @@
+// var Oname = document.getElementById("OrdName");
+// var Oemail= document.getElementById("OrdEmail");
+// var Olocation= document.getElementById("OrdLocation");
+// var Oqty= document.getElementById("OrdQty");
+
+// let OnameValue;
+// let OemailValue;
+// let OlocationValue;
+// let OqtyValue;
+
+
+
+// // if(!(Oname=='' && Oemail=='' &&location=='' && Oqty=='')){
+// //     console.log(Oname);
+// //      console.log(Oemail);
+// //       console.log(Olocation);
+// //        console.log(Oqty);
+// //     document.getElementById("Obtn").disabled=false;
+// // }
+// // console.log(document.getElementById("OrdName"));
+
+
+
+
+
+// function Datastore(e){
+//     if(e.target.value!=''){
+//         document.getElementById("Obtn").disabled=false;
+//     }
+//     else{
+//         document.getElementById("Obtn").disabled=true;
+//     }
+
+        
+        
+//         if(e.target == Oname){
+//             console.log("Enter to if");
+            
+//              OnameValue = e.target.value;
+
+//              console.log("value is: ",OnameValue);
+//         }
+   
+    
+    
+// }
+
+
+function clickBtnTable(){
+
+    alert("Table Booked Successfully")
+}
+
